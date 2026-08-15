a73690c1a79d644aabf96701445b84030c934d78
MiguelGFerreira <miguel.gfe@outlook.com>
2026-08-15 18:17:29 -0300

    chore: setup spring boot 3.5 with java 21 and base configuration

---
+++ src/main/java/tech/migueldev/coffeewarehouse/CoffeeWarehouseApplication.java
@@ -0,0 +1,12 @@
+package tech.migueldev.coffeewarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeWarehouseApplication.class, args);
+    }
+}
